8b49d0afcb15246e67cdbfa2778a795aeeb7bdc3
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2019-08-04 20:49:09 +0200

    Update Person.java file

--- Persons/Person.java
+++ Persons/Person.java
@@ -1,5 +1,19 @@
 package Persons;
 
 public class Person {
-    
+    private String firstName;
+    private String lastName;
+
+    Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
