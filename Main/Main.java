db015e4c0d3d5fbd6134d694ff2a8538cb86dbe2
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2019-08-04 21:16:16 +0200

    Update Main.java file

--- Main/Main.java
+++ Main/Main.java
@@ -1,7 +1,18 @@
 package Main;
 
+import Persons.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Person[] person = new Person[2];
+
+        person[0] = new Worker("Łukasz", "Zbrzeski", 5000);
+        person[1] = new Student("Łukasz", "Zbrzeski", "Example");
+
+        for (Person persons : person) {
+            if (persons instanceof Person) {
+                System.out.println(persons.toString() + "\n");
+            }
+        }
     }
 }
